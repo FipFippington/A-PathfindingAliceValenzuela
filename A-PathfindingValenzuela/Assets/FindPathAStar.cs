6822a71c6850950d921902b239fcd1e66a7ce074
FipFippington <116121197+FipFippington@users.noreply.github.com>
2023-12-07 13:47:23 -0800

    Started making pathfinding script
    We gyatt to find the path

---
+++ A-PathfindingValenzuela/Assets/FindPathAStar.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+
+public class PathMaker
+{
+    public MapLocation location;
+    public float G;
+    public float H;
+    public float F;
+    public GameObject marker;
+    public PathMaker parent;
+}
+
+public class FindPathAStar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
