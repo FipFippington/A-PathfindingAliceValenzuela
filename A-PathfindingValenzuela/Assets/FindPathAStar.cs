09f0098aa8f592edc6560c549e79ea42be47d457
FipFippington <116121197+FipFippington@users.noreply.github.com>
2023-12-15 09:05:42 -0800

    did le thing
    yipe

--- A-PathfindingValenzuela/Assets/FindPathAStar.cs
+++ A-PathfindingValenzuela/Assets/FindPathAStar.cs
@@ -12,10 +12,98 @@ public class PathMaker
     public float F;
     public GameObject marker;
     public PathMaker parent;
+
+    public PathMaker(MapLocation l, float g, float h, float f, GameObject marker, PathMaker p)
+    {
+        location = l;
+        G = g;
+        H = h;
+        F = f;
+        this.marker = marker;
+        parent = p;
+    }
+    public override bool Equals(object obj)
+    {
+        if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+        {
+            return false;
+        }
+        else
+        {
+            return location.Equals(((PathMaker)obj).location);
+        }
+    }
+    public override int GetHashCode()
+    {
+        return 0;
+    }
 }
 
 public class FindPathAStar : MonoBehaviour
 {
+    public Maze maze;
+    public Material closedMaterial;
+    public Material openMaterial;
+
+    List<PathMaker> open = new List<PathMaker>();
+    List<PathMaker> closed = new List<PathMaker>();
+
+    public GameObject start;
+    public GameObject end;
+    public GameObject pathP;
+
+    PathMaker goalNode;
+    PathMaker startNode;
+
+    PathMaker lastPos;
+    bool done = false;
+
+    void removeAllMarkers()
+    {
+        GameObject[] markers = GameObject.FindGameObjectsWithTag("marker");
+        foreach (GameObject m in markers)
+            Destroy(m);
+    }
+
+    void beginSearch()
+    {
+        done = false;
+        removeAllMarkers();
+
+        List<MapLocation> locations = new List<MapLocation>();
+        for (int z = 1; z < maze.depth - 1; z++)
+            for (int x = 1; x < maze.width - 1; x++)
+            {
+                if (maze.map[x, z] != 1)
+                    locations.Add(new MapLocation(x, z));
+            }
+
+        locations.Shuffle();
+
+        Vector3 startLoacation = new Vector3(locations[0].x * maze.scale, 0, locations[0].z * maze.scale);
+        startNode = new PathMaker(new MapLocation(locations[0].x, locations[0].z), 0, 0, 0,
+            Instantiate(start, startLoacation, Quaternion.identity), null);
+
+        Vector3 goalLoacation = new Vector3(locations[1].x * maze.scale, 0, locations[1].z * maze.scale);
+        startNode = new PathMaker(new MapLocation(locations[0].x, locations[1].z), 0, 0, 0,
+            Instantiate(end, goalLoacation, Quaternion.identity), null);
+
+        open.Clear();
+        closed.Clear();
+        open.Add(startNode);
+        lastPos = startNode;
+    }
+
+    void Search(PathMaker thisNode)
+    {
+        if (thisNode.Equals(goalNode)) { done = true;  return; }
+
+        foreach (MapLocation dir in maze.directions)
+        {
+
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +113,6 @@ public class FindPathAStar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P)) beginSearch();
     }
 }
