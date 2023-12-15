044c7fa817f1193014582602870cdefc0f70a810
FipFippington <116121197+FipFippington@users.noreply.github.com>
2023-12-15 13:53:48 -0800

    Done
    cvhbn hncvgfhnbt rdgvhbjnkgmkv

--- A-PathfindingValenzuela/Assets/FindPathAStar.cs
+++ A-PathfindingValenzuela/Assets/FindPathAStar.cs
@@ -3,116 +3,181 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
+public class PathMarker {
 
-public class PathMaker
-{
     public MapLocation location;
-    public float G;
-    public float H;
-    public float F;
+    public float G, H, F;
     public GameObject marker;
-    public PathMaker parent;
+    public PathMarker parent;
 
-    public PathMaker(MapLocation l, float g, float h, float f, GameObject marker, PathMaker p)
-    {
+    public PathMarker(MapLocation l, float g, float h, float f, GameObject m, PathMarker p) {
+
         location = l;
         G = g;
         H = h;
         F = f;
-        this.marker = marker;
+        marker = m;
         parent = p;
     }
-    public override bool Equals(object obj)
-    {
+
+    public override bool Equals(object obj) {
+
         if ((obj == null) || !this.GetType().Equals(obj.GetType()))
-        {
             return false;
-        }
         else
-        {
-            return location.Equals(((PathMaker)obj).location);
-        }
+            return location.Equals(((PathMarker)obj).location);
     }
-    public override int GetHashCode()
-    {
+
+    public override int GetHashCode() {
+
         return 0;
     }
 }
 
-public class FindPathAStar : MonoBehaviour
-{
+public class FindPathAStar : MonoBehaviour {
+
     public Maze maze;
     public Material closedMaterial;
     public Material openMaterial;
-
-    List<PathMaker> open = new List<PathMaker>();
-    List<PathMaker> closed = new List<PathMaker>();
-
     public GameObject start;
     public GameObject end;
     public GameObject pathP;
 
-    PathMaker goalNode;
-    PathMaker startNode;
-
-    PathMaker lastPos;
+    PathMarker startNode;
+    PathMarker goalNode;
+    PathMarker lastPos;
     bool done = false;
+    bool hasStarted = false;
 
-    void removeAllMarkers()
-    {
+    List<PathMarker> open = new List<PathMarker>();
+    List<PathMarker> closed = new List<PathMarker>();
+
+    void RemoveAllMarkers() {
+
         GameObject[] markers = GameObject.FindGameObjectsWithTag("marker");
-        foreach (GameObject m in markers)
-            Destroy(m);
+
+        foreach (GameObject m in markers) Destroy(m);
     }
 
-    void beginSearch()
-    {
+    void BeginSearch() {
+
         done = false;
-        removeAllMarkers();
+        RemoveAllMarkers();
 
         List<MapLocation> locations = new List<MapLocation>();
-        for (int z = 1; z < maze.depth - 1; z++)
-            for (int x = 1; x < maze.width - 1; x++)
-            {
-                if (maze.map[x, z] != 1)
-                    locations.Add(new MapLocation(x, z));
-            }
 
+        for (int z = 1; z < maze.depth - 1; ++z) {
+            for (int x = 1; x < maze.width - 1; ++x) {
+
+                if (maze.map[x, z] != 1) {
+
+                    locations.Add(new MapLocation(x, z));
+                }
+            }
+        }
         locations.Shuffle();
 
-        Vector3 startLoacation = new Vector3(locations[0].x * maze.scale, 0, locations[0].z * maze.scale);
-        startNode = new PathMaker(new MapLocation(locations[0].x, locations[0].z), 0, 0, 0,
-            Instantiate(start, startLoacation, Quaternion.identity), null);
+        Vector3 startLocation = new Vector3(locations[0].x * maze.scale, 0.0f, locations[0].z * maze.scale);
+        startNode = new PathMarker(new MapLocation(locations[0].x, locations[0].z),
+            0.0f, 0.0f, 0.0f, Instantiate(start, startLocation, Quaternion.identity), null);
 
-        Vector3 goalLoacation = new Vector3(locations[1].x * maze.scale, 0, locations[1].z * maze.scale);
-        startNode = new PathMaker(new MapLocation(locations[0].x, locations[1].z), 0, 0, 0,
-            Instantiate(end, goalLoacation, Quaternion.identity), null);
+        Vector3 endLocation = new Vector3(locations[1].x * maze.scale, 0.0f, locations[1].z * maze.scale);
+        goalNode = new PathMarker(new MapLocation(locations[1].x, locations[1].z),
+            0.0f, 0.0f, 0.0f, Instantiate(end, endLocation, Quaternion.identity), null);
 
         open.Clear();
         closed.Clear();
+
         open.Add(startNode);
         lastPos = startNode;
     }
 
-    void Search(PathMaker thisNode)
-    {
-        if (thisNode.Equals(goalNode)) { done = true;  return; }
+    void Search(PathMarker thisNode) {
 
-        foreach (MapLocation dir in maze.directions)
-        {
+        if (thisNode.Equals(goalNode)) {
 
+            done = true;
+            return;
         }
+
+        foreach (MapLocation dir in maze.directions) {
+
+            MapLocation neighbour = dir + thisNode.location;
+
+            if (maze.map[neighbour.x, neighbour.z] == 1) continue;
+            if (neighbour.x < 1 || neighbour.x >= maze.width || neighbour.z < 1 || neighbour.z >= maze.depth) continue;
+            if (IsClosed(neighbour)) continue;
+
+            float g = Vector2.Distance(thisNode.location.ToVector(), neighbour.ToVector()) + thisNode.G;
+            float h = Vector2.Distance(neighbour.ToVector(), goalNode.location.ToVector());
+            float f = g + h;
+
+            GameObject pathBlock = Instantiate(pathP, new Vector3(neighbour.x * maze.scale, 0.0f, neighbour.z * maze.scale), Quaternion.identity);
+
+            TextMesh[] values = pathBlock.GetComponentsInChildren<TextMesh>();
+
+            values[0].text = "G: " + g.ToString("0.00");
+            values[1].text = "H: " + h.ToString("0.00");
+            values[2].text = "F: " + f.ToString("0.00");
+
+            if (!UpdateMarker(neighbour, g, h, f, thisNode)) {
+
+                open.Add(new PathMarker(neighbour, g, h, f, pathBlock, thisNode));
+            }
+        }
+        open = open.OrderBy(p => p.F).ToList<PathMarker>();
+        PathMarker pm = (PathMarker)open.ElementAt(0);
+        closed.Add(pm);
+
+        open.RemoveAt(0);
+        pm.marker.GetComponent<Renderer>().material = closedMaterial;
+
+        lastPos = pm;
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    bool UpdateMarker(MapLocation pos, float g, float h, float f, PathMarker prt) {
+
+        foreach (PathMarker p in open) {
+
+            if (p.location.Equals(pos)) {
+
+                p.G = g;
+                p.H = h;
+                p.F = f;
+                p.parent = prt;
+                return true;
+            }
+        }
+        return false;
     }
 
-    // Update is called once per frame
-    void Update()
+    bool IsClosed(MapLocation marker) {
+
+        foreach (PathMarker p in closed) {
+
+            if (p.location.Equals(marker)) return true;
+        }
+        return false;
+    }
+
+    void GetPath()
     {
-        if (Input.GetKeyDown(KeyCode.P)) beginSearch();
+        RemoveAllMarkers();
+        PathMarker begin = lastPos;
+
+        while (!startNode.Equals(begin) && begin != null)
+        {
+            Instantiate(pathP, new Vector3(begin.location.x * maze.scale, 0, begin.location.z * maze.scale), Quaternion.identity);
+            begin = begin.parent;
+        }
+        Instantiate(pathP, new Vector3(startNode.location.x * maze.scale, 0, startNode.location.z * maze.scale), Quaternion.identity);
+    }
+
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.P))
+            BeginSearch();
+        if (Input.GetKeyDown(KeyCode.C) && !done) Search(lastPos);
+        if (Input.GetKeyDown(KeyCode.M)) GetPath();
     }
 }
